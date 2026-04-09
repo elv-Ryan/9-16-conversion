ca9574afdd60b90f62a08b15b77b497524f1181d
elv-joeA <joe.anakata@eluv.io>
2026-04-09 12:12:20 +0000

    boxes start at 1 not 0

--- run.py
+++ run.py
@@ -575,10 +575,10 @@ def main():
                         "frame_info": {
                             "frame_idx": j,
                             "box": {
-                                "x0": x0,
-                                "y0": y0,
-                                "x1": x1,
-                                "y1": y1,
+                                "x1": x0,
+                                "y1": y0,
+                                "x2": x1,
+                                "y2": y1,
                             }
                         },
                         "source_media": input_files[0]
